abc0786eb8727ee4235b31ec0ab32de7a0f90e70
Martina Nováková <martina@logici.cz>
2025-10-17 10:47:33 +0200

    Add condition for rounding note

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ let fahrenheit = Number(prompt("Zadej teplotu ve stupních Fahrenheita: "));
 
 let celsius = 5 * (fahrenheit - 32) / 9;
 
-document.body.innerHTML += fahrenheit + '°F = ' + Math.round(celsius * 100)/100 + '°C' + ' (zaokrouhleno na dvě desetinná mista)'
+document.body.innerHTML += fahrenheit + '°F = ' + Math.round(celsius * 100)/100 + '°C'
+
+if (Math.round(celsius * 100) != celsius * 100) {
+    document.body.innerHTML += ' (zaokrouhleno na dvě desetinná mista)'
+}
