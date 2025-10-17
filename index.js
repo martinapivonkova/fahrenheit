fccba05b3e920fb228be24d00fe7a5ca25652bd1
Martina Nováková <martina@logici.cz>
2025-10-17 15:04:46 +0200

    Add simple CSS style

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let fahrenheit = Number(prompt("Zadej teplotu ve stupních Fahrenheita: "));
 
 let celsius = 5 * (fahrenheit - 32) / 9;
 
-document.body.innerHTML += fahrenheit + '°F = ' + Math.round(celsius * 100)/100 + '°C'
+document.body.innerHTML += '<div class=calc>' + fahrenheit + '°F = ' + Math.round(celsius * 100)/100 + '°C<div/>'
 
 if (Math.round(celsius * 100) != celsius * 100) {
-    document.body.innerHTML += ' (zaokrouhleno na dvě desetinná mista)'
+    document.body.innerHTML += '<div class=calc-note>(zaokrouhleno na dvě desetinná mista)<div>'
 }
